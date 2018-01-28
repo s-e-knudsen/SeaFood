211a21c15c6b3c820d82efece76e37025f966e78
SorenEKnudsen <soren.egede.knudsen@gmail.com>
2018-01-28 15:41:50 +0100

    CoreML samle start. Can not take a photo and analyse it with CoreML. (The result is in the debug).

--- SeaFood/ViewController.swift
+++ SeaFood/ViewController.swift
@@ -7,18 +7,77 @@
 //
 
 import UIKit
+import CoreML
+import Vision
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
+    
+    @IBOutlet weak var imageView: UIImageView!
+    let imagePicker = UIImagePickerController()
+    
+    
+    @IBAction func cameraTapped(_ sender: UIBarButtonItem) {
+        present(imagePicker, animated: true, completion: nil)
+        
+        
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        
+        if let userPickedimage = info[UIImagePickerControllerOriginalImage] as? UIImage {
+            imageView.image = userPickedimage
+            
+            
+            guard let ciimage = CIImage(image: userPickedimage) else {
+                fatalError("Could not convert image")
+            }
+            
+            detect(image: ciimage)
+        }
+        
+        imagePicker.dismiss(animated: true, completion: nil)
+        
+       
+    }
+    
+    //MARK: - Detect the image using ML CoreML
+    func detect(image: CIImage) {
+        
+        guard let model = try? VNCoreMLModel(for: Inceptionv3().model) else {
+            fatalError("Loading CoreML Model Failed")
+        }
+        let request = VNCoreMLRequest(model: model) { (recuest, error) in
+            guard let results = recuest.results as? [VNClassificationObservation] else {
+                fatalError("Error getting the result")
+            }
+            
+            print(results)
+        }
+        
+        let handler = VNImageRequestHandler(ciImage: image)
+        
+        do {
+            try handler.perform([request])
+        } catch {
+            print(error)
+        }
+        
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+       
+        //Image picker setup
+        imagePicker.delegate = self
+        imagePicker.sourceType = .camera
+        imagePicker.allowsEditing = false
+        
+        
+        
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
+  
 
 
 }
